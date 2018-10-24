3444c3cca84099eed2607b39be4864fc121e58e1
Alexey Brazhe <brazhe@gmail.com>
2018-10-24 17:07:58 +0300

    convert to Py3

--- morphsnakes.py
+++ morphsnakes.py
@@ -41,13 +41,13 @@ class fcycle(object):
         self.funcs = cycle(iterable)
     
     def __call__(self, *args, **kwargs):
-        f = self.funcs.next()
+        f = next(self.funcs)
         return f(*args, **kwargs)
     
 
 # SI and IS operators for 2D and 3D.
 _P2 = [np.eye(3), np.array([[0,1,0]]*3), np.flipud(np.eye(3)), np.rot90([[0,1,0]]*3)]
-_P3 = [np.zeros((3,3,3)) for i in xrange(9)]
+_P3 = [np.zeros((3,3,3)) for i in range(9)]
 
 _P3[0][:,:,1] = 1
 _P3[1][:,1,:] = 1
@@ -68,12 +68,12 @@ def SI(u):
     elif np.ndim(u) == 3:
         P = _P3
     else:
-        raise ValueError, "u has an invalid number of dimensions (should be 2 or 3)"
+        raise ValueError("u has an invalid number of dimensions (should be 2 or 3)")
     
     if u.shape != _aux.shape[1:]:
         _aux = np.zeros((len(P),) + u.shape)
     
-    for i in xrange(len(P)):
+    for i in range(len(P)):
         _aux[i] = binary_erosion(u, P[i])
     
     return _aux.max(0)
@@ -86,12 +86,12 @@ def IS(u):
     elif np.ndim(u) == 3:
         P = _P3
     else:
-        raise ValueError, "u has an invalid number of dimensions (should be 2 or 3)"
+        raise ValueError("u has an invalid number of dimensions (should be 2 or 3)")
     
     if u.shape != _aux.shape[1:]:
         _aux = np.zeros((len(P),) + u.shape)
     
-    for i in xrange(len(P)):
+    for i in range(len(P)):
         _aux[i] = binary_dilation(u, P[i])
     
     return _aux.min(0)
@@ -155,7 +155,7 @@ class MorphACWE(object):
         u = self._u
         
         if u is None:
-            raise ValueError, "the levelset function is not set (use set_levelset)"
+            raise ValueError("the levelset function is not set (use set_levelset)")
         
         data = self.data
         
@@ -176,7 +176,7 @@ class MorphACWE(object):
         res[aux > 0] = 0
         
         # Smoothing.
-        for i in xrange(self.smoothing):
+        for i in range(self.smoothing):
             res = curvop(res)
         
         self._u = res
@@ -185,12 +185,12 @@ class MorphACWE(object):
         """Run several iterations of the morphological Chan-Vese method."""
         autostopped = False
         history = np.zeros((3,)+self._u.shape)
-        for i in xrange(iterations):
+        for i in range(iterations):
             if autostop:
                 if i > 2:
                     x = [np.sum((self._u-c)**2) for c in history]
                     if np.min(x) < 1e-6:
-                        print 'reached convergence at iteration %d'%i
+                        print('reached convergence at iteration %d'%i)
                         autostopped = True
                         break
             history[:-1] = history[1:]
@@ -272,7 +272,7 @@ class MorphGAC(object):
         v = self._v
         
         if u is None:
-            raise ValueError, "the levelset is not set (use set_levelset)"
+            raise ValueError("the levelset is not set (use set_levelset)")
         
         res = np.copy(u)
         
@@ -293,14 +293,14 @@ class MorphGAC(object):
         res[aux < 0] = 0
         
         # Smoothing.
-        for i in xrange(self.smoothing):
+        for i in range(self.smoothing):
             res = curvop(res)
         
         self._u = res
     
     def run(self, iterations):
         """Run several iterations of the morphological snakes method."""
-        for i in xrange(iterations):
+        for i in range(iterations):
             self.step()
     
 
@@ -341,7 +341,7 @@ def evolve_visual(msnake, levelset=None, num_iters=20, background=None):
     ppl.pause(0.001)
     
     # Iterate.
-    for i in xrange(num_iters):
+    for i in range(num_iters):
         # Evolve.
         msnake.step()
         
@@ -380,7 +380,7 @@ def evolve_visual3d(msnake, levelset=None, num_iters=20):
     mlab.pipeline.image_plane_widget(src, plane_orientation='x_axes', colormap='gray')
     cnt = mlab.contour3d(msnake.levelset, contours=[0.5])
     
-    for i in xrange(num_iters):
+    for i in range(num_iters):
         msnake.step()
         cnt.mlab_source.scalars = msnake.levelset
     
